87557fdc18675e145d4efd1b7582efe3a8e89ddb
JohnLewis9904 <90780139+JohnLewis9904@users.noreply.github.com>
2022-09-09 12:10:10 -0500

    Set red color theme

--- app/src/main/java/com/example/practicingthemes/MainActivity.java
+++ app/src/main/java/com/example/practicingthemes/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.practicingthemes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    TextView myYellowText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText username = findViewById(R.id.editTextTextPersonName);
+        EditText password = findViewById(R.id.editTextTextPassword);
+        TextView myYellowText = findViewById(R.id.textView);
+    }
+
+    public void clickMe(View v) {
+        Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+        if(parseExist(v)){
+            startActivity(intent);
+        }else{
+            myYellowText.setText("Please enter a username and password.");
+        }
+    }
+
+    public boolean parseExist(View v){
+        return (!username.getText().toString().equals("")) && (!password.getText().toString().equals(""));
     }
 }
